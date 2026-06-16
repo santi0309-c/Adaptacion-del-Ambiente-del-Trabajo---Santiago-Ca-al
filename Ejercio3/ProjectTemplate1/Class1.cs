2e5323ae335778c3b5df986edd02b8a20b32f063
santi0309-c <sanbar2030@gmail.com>
2026-06-16 11:16:51 -0300

    Ejercicio 3 terminado

---
+++ Ejercio3/ProjectTemplate1/Class1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectTemplate1
+{
+    interface IJugador
+    {
+        bool Correr(int min);
+        bool Cansado();
+        void Descansar(int min);
+    }
+
+    class JugadorAmateur : IJugador
+    {
+        private int minCorridos;
+        private const int MAX = 20;
+
+        public JugadorAmateur()
+        {
+            minCorridos = 0;
+        }
+
+        public bool Correr(int min)
+        {
+            if (Cansado())
+            {
+                Console.WriteLine("El amateur está muy cansado, no aguanta más!");
+                return false;
+            }
+
+            int minDisponibles = MAX - minCorridos;
+
+            if (min <= minDisponibles)
+            {
+                minCorridos += min;
+                Console.WriteLine($"Amateur corrió {min} min sin problema.");
+                return true;
+            }
+            else
+            {
+                minCorridos = MAX;
+                Console.WriteLine($"Amateur se cansó después de {minDisponibles} min.");
+                return false;
+            }
+        }
+
+        public bool Cansado()
+        {
+            return minCorridos >= MAX;
+        }
+
+        public void Descansar(int min)
+        {
+            minCorridos -= min;
+            if (minCorridos < 0)
+                minCorridos = 0;
+            Console.WriteLine($"Amateur descansó {min} min, se recuperó.");
+        }
+    }
+
+    class JugadorProfesional : IJugador
+    {
+        private int minCorridos;
+        private const int MAX = 40;
+
+        public JugadorProfesional()
+        {
+            minCorridos = 0;
+        }
+
+        public bool Correr(int min)
+        {
+            if (Cansado())
+            {
+                Console.WriteLine("El profesional está cansado, necesita parar!");
+                return false;
+            }
+
+            int minDisponibles = MAX - minCorridos;
+
+            if (min <= minDisponibles)
+            {
+                minCorridos += min;
+                Console.WriteLine($"Profesional corrió {min} min fácil.");
+                return true;
+            }
+            else
+            {
+                minCorridos = MAX;
+                Console.WriteLine($"Profesional se cansó después de {minDisponibles} min.");
+                return false;
+            }
+        }
+
+        public bool Cansado()
+        {
+            return minCorridos >= MAX;
+        }
+
+        public void Descansar(int min)
+        {
+            minCorridos -= min;
+            if (minCorridos < 0)
+                minCorridos = 0;
+            Console.WriteLine($"Profesional descansó {min} min.");
+        }
+    }
+
+    class PruebaJugadores
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== PROBANDO JUGADORES ===\n");
+
+            JugadorAmateur pepe = new JugadorAmateur();
+            JugadorProfesional messi = new JugadorProfesional();
+
+            Console.WriteLine("--- Amateur (Pepe) ---");
+            pepe.Correr(15);
+            Console.WriteLine($"¿Cansado? {pepe.Cansado()}");
+
+            pepe.Correr(10);
+            Console.WriteLine($"¿Cansado? {pepe.Cansado()}");
+
+            pepe.Correr(5);
+            pepe.Descansar(20);
+            Console.WriteLine($"¿Cansado después de descansar? {pepe.Cansado()}\n");
+
+            Console.WriteLine("--- Profesional (Messi) ---");
+            messi.Correr(30);
+            Console.WriteLine($"¿Cansado? {messi.Cansado()}");
+
+            messi.Correr(15);
+            Console.WriteLine($"¿Cansado? {messi.Cansado()}");
+
+            messi.Descansar(10);
+            messi.Correr(5);
+        }
+    }
+}
---
+++ Ejercio3/ProjectTemplate1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProjectTemplate1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("ProjectTemplate1")]
+[assembly: AssemblyCopyright("Copyright © Company 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74dfcfc7-50f7-4e91-9c27-f964389ec6f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
